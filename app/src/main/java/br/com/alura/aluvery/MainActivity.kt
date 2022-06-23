35368062243c371034a18e6f6acb84c62f9870cf
Alex Felipe <alexfelipevieira@gmail.com>
2022-06-23 10:08:56 -0300

    3.2

--- app/src/main/java/br/com/alura/aluvery/MainActivity.kt
+++ app/src/main/java/br/com/alura/aluvery/MainActivity.kt
@@ -8,19 +8,25 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.BottomCenter
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.datasource.LoremIpsum
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import br.com.alura.aluvery.ui.theme.AluveryTheme
 import br.com.alura.aluvery.ui.theme.Purple200
 import br.com.alura.aluvery.ui.theme.Purple500
@@ -50,20 +56,40 @@ fun ProductItem() {
         Box(
             modifier = Modifier
                 .height(100.dp)
-                .background(brush = Brush.horizontalGradient(colors = listOf(
-                    Purple500, Teal200
-                )))
+                .background(
+                    brush = Brush.horizontalGradient(
+                        colors = listOf(
+                            Purple500, Teal200
+                        )
+                    )
+                )
                 .fillMaxWidth()
-        )
-        Image(
-            painter = painterResource(id = R.drawable.ic_launcher_background),
-            contentDescription = null,
-            Modifier.size(100.dp)
-                .offset(y = (-50).dp)
-                .clip(shape = CircleShape)
-                .align(CenterHorizontally)
-        )
-        Text(text = "Texto 1")
-        Text(text = "Texto 2")
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_launcher_background),
+                contentDescription = null,
+                Modifier
+                    .size(100.dp)
+                    .offset(y = 50.dp)
+                    .clip(shape = CircleShape)
+                    .align(BottomCenter)
+            )
+        }
+        Spacer(modifier = Modifier.height(50.dp))
+        Column(Modifier.padding(16.dp)) {
+            Text(
+                text = LoremIpsum(50).values.first(),
+                fontSize = 18.sp,
+                fontWeight = FontWeight(700),
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis
+            )
+            Text(
+                text = "R$ 14,99",
+                Modifier.padding(top = 8.dp),
+                fontSize = 14.sp,
+                fontWeight = FontWeight(400)
+            )
+        }
     }
 }
